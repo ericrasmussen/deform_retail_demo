3ad5896dffa1e8def23c07ea2072d0763f296f66
Eric Rasmussen <ericrasmussen@gmail.com>
2013-05-23 23:00:21 -0700

    configure a basic contact form view

--- deformretail/views.py
+++ deformretail/views.py
@@ -1,12 +1,7 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'deformretail'}
-
-
-@view_config(route_name='contact', renderer='templates/contact.mako')
+@view_config(route_name='contact', renderer='contact.mako')
 def contact(request):
     """
     A basic contact form with field validation by deform.
