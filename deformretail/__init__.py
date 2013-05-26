342b25aecd3762c43ad35dd453662487c39677ae
Eric Rasmussen <ericrasmussen@gmail.com>
2013-05-25 23:37:16 -0700

    meaningful spacing

--- deformretail/__init__.py
+++ deformretail/__init__.py
@@ -7,10 +7,15 @@ def main(global_config, **settings):  # pragma no cover
     """ This function returns a Pyramid WSGI application.
     """
     session_factory = UnencryptedCookieSessionFactoryConfig('not-so-secret')
+
     config = Configurator(settings=settings, session_factory=session_factory)
+
     config.add_static_view('static', 'static', cache_max_age=3600)
+
     config.add_route('home', '/')
     config.add_route('contact', '/contact')
     config.add_route('account', '/account')
+
     config.scan()
+
     return config.make_wsgi_app()
